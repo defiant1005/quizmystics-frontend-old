c4159c66d8525f09c8c9b6b40dd17025911c4c59
r.idiyatov <r.idiyatov@iilab.ru>
2023-05-27 23:33:57 +0300

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import "./assets/main.css";
-
+import "./scss/main.scss";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,14 +3,14 @@ import { ref } from "vue";
 import { io } from "socket.io-client";
 
 const inputValue = ref("");
-const socket = io("http://localhost:3000/", {
+const socket = io("http://192.168.0.101:3000/", {
   withCredentials: true,
 });
 
-const messages = [];
+const messages = ref([]);
 
 socket.on("chat message", function (msg) {
-  messages.push(msg);
+  messages.value.push(msg);
 });
 
 const submit = () => {
@@ -39,14 +39,66 @@ const submit = () => {
 
 <template>
   <div class="home-page">
-    <form @submit.prevent="submit">
-      <input v-model="inputValue" type="text" />
-      <button type="submit">Отправить</button>
+    <div class="home-page__main main">
+      <ul class="main__list list">
+        <li v-for="(msg, index) in messages" :key="index" class="list__item">
+          {{ msg }}
+        </li>
+      </ul>
+    </div>
+
+    <form class="home-page__form form" @submit.prevent="submit">
+      <input
+        v-model="inputValue"
+        type="text"
+        class="form__input"
+        placeholder="Введите текст"
+      />
+      <button type="submit" class="form__btn">Отправить</button>
     </form>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .home-page {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 30px;
+  background: rgba(118, 118, 241, 0.3);
+
+  .main {
+    flex: 1 0 auto;
+    max-height: calc(100vh - 160px);
+    overflow: auto;
+
+    .list {
+      &__item {
+        border-bottom: 1px solid black;
+        padding: 8px;
+        margin-bottom: 8px;
+      }
+    }
+  }
+
+  .form {
+    flex: 0 0 auto;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    &__input {
+      height: 50px;
+      width: 100%;
+      padding: 10px;
+      border-radius: 8px;
+      border: 1px solid black;
+      transition: all 20s ease;
+    }
+    &__btn {
+      height: 50px;
+    }
+  }
 }
 </style>
