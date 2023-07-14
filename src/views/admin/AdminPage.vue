e645ea3f98e5abb1e0c9a0caf6dd67e89ae5e466
r.idiyatov <r.idiyatov@iilab.ru>
2023-07-14 17:53:46 +0300

    feat(user): add edit user logic

--- src/views/admin/AdminPage.vue
+++ src/views/admin/AdminPage.vue
@@ -19,12 +19,6 @@ export default defineComponent({
     };
   },
 
-  methods: {
-    closeModalHandler() {
-      console.log(123);
-    },
-  },
-
   mounted() {
     this.categoriesStore.getCategories();
   },
@@ -36,36 +30,10 @@ export default defineComponent({
     <AdminNavigation />
 
     <router-view></router-view>
-
-    <!--    <button-->
-    <!--      type="button"-->
-    <!--      class="btn btn-primary"-->
-    <!--      data-bs-toggle="modal"-->
-    <!--      data-bs-target="#exampleModal"-->
-    <!--    >-->
-    <!--      Создать категорию-->
-    <!--    </button>-->
-
-    <!--    <MainModal :title="'Создать категорию'">-->
-    <!--      <AdminModalFormCategory @closeModal="closeModalHandler" />-->
-    <!--    </MainModal>-->
   </div>
 </template>
 
 <style lang="scss" scoped>
 .admin-page {
-  //display: flex;
-  //align-items: center;
-  //justify-content: center;
-  //height: 100%;
-  //flex-direction: column;
-  //gap: 20px;
-  //position: relative;
-
-  &__exit {
-    //position: absolute;
-    //top: 50px;
-    //right: 50px;
-  }
 }
 </style>
